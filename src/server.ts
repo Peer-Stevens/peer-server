a105115626b97877a1dff56ef01ed101a688c57d
Andrew Jones <aej11@hotmail.com>
2021-09-14 17:33:39 -0400

    Added app.listen to actually start express :)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import { example } from './rest/example'
+import { example } from "./rest/example";
 import express from "express";
 const app = express();
 const port = 3030;
 
 app.get("/example", example);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
