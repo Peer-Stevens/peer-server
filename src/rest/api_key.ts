335becf0237867c3dedc592ab7d909e0e7b0787f
David Carpenter <carpenterd777@gmail.com>
2021-09-24 20:53:51 -0400

    add maps api route

--- src/rest/api_key.ts
+++ src/rest/api_key.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 import StatusCode from "../utils/status_code";
 
 export const maps_api = (req: Request, res: Response) => {
-	res.status(StatusCode.OK).json({ key: "test" });
+	res.status(StatusCode.OK).json({ key: process.env.MAPS_API });
 };
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { example } from "./rest/example";
+import { maps_api } from "./rest/api_key";
 import express from "express";
 import dotenv from "dotenv";
 const app = express();
@@ -7,6 +8,7 @@ const port = 3030;
 dotenv.config();
 
 app.get("/example", example);
+app.get("/api/maps", maps_api);
 
 app.listen(port, () => {
 	console.log(`App listening at http://localhost:${port}`);
