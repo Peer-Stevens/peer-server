955c51354e6ad556fc661c6c67de8c11e71a44e2
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-09-26 21:32:15 -0400

    added types and removed schema.json

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,7 +1,32 @@
 import { ObjectId } from "mongodb";
+export interface User {
+	_id?: ObjectId;
+	isBlind: boolean;
+	firstName: string;
+	lastName: string;
+	email: string;
+	reviews: Array<ObjectId> | Array<null>;
+}
+
+export interface Review {
+	_id?: ObjectId;
+	userID: ObjectId;
+	// made placeID an ObjectID for now until we figure out what type this will end up being
+	placeID: ObjectId;
+	brailleRating: number | null;
+	fontSizeRating: number | null;
+	staffHelpfulnessRating: number | null;
+	openessOfSpaceRating: number | null;
+	comment: string;
+	dateCreated: Date;
+}
 
 export interface Place {
+	// made this an ObjectID for now until we figure out what type this will end up being
 	_id?: ObjectId;
-	name: string;
-	establishmentType: string;
+	avgBrailleRating: number | "No ratings have been submitted yet.";
+	avgFontSizeRating: number | "No ratings have been submitted yet.";
+	avgStaffHelpfulnessRating: number | "No ratings have been submitted yet.";
+	avgOpenessOfSpaceRating: number | "No ratings have been submitted yet.";
+	comments: Array<string>;
 }
