d5f2df51e44d53a28de31d1977ea0f89d2e5fcd2
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-09-28 15:17:17 -0400

    edits to types from code review

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,11 +1,11 @@
-import { ObjectId } from "mongodb";
+import type { ObjectId } from "mongodb";
 export interface User {
 	_id?: ObjectId;
-	isBlind: boolean;
+	isBlindMode: boolean;
 	firstName: string;
 	lastName: string;
 	email: string;
-	reviews: Array<ObjectId> | Array<null>;
+	reviews: Array<ObjectId>;
 }
 
 export interface Review {
@@ -13,10 +13,10 @@ export interface Review {
 	userID: ObjectId;
 	// made placeID an ObjectID for now until we figure out what type this will end up being
 	placeID: ObjectId;
-	brailleRating: number | null;
-	fontSizeRating: number | null;
-	staffHelpfulnessRating: number | null;
-	openessOfSpaceRating: number | null;
+	braille: number | null;
+	fontReadability: number | null;
+	staffHelpfulness: number | null;
+	openessOfSpace: number | null;
 	comment: string | null;
 	dateCreated: Date;
 }
@@ -24,9 +24,9 @@ export interface Review {
 export interface Place {
 	// made this an ObjectID for now until we figure out what type this will end up being
 	_id?: ObjectId;
-	avgBrailleRating: number | "No ratings have been submitted yet.";
-	avgFontSizeRating: number | "No ratings have been submitted yet.";
-	avgStaffHelpfulnessRating: number | "No ratings have been submitted yet.";
-	avgOpenessOfSpaceRating: number | "No ratings have been submitted yet.";
-	comments: Array<string> | Array<null>;
+	avgBraille: number;
+	avgFontReadability: number;
+	avgStaffHelpfulness: number;
+	avgOpenessOfSpace: number;
+	comments: Array<string>;
 }
