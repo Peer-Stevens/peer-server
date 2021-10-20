e34a75dac7d634355de4f0274783bf7403f7dbb1
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-10-19 22:32:07 -0400

    updated mongoCollections.ts

--- src/db/mongoCollections.ts
+++ src/db/mongoCollections.ts
@@ -1,17 +1,15 @@
 import { dbConnection } from "./mongoConnection";
 import type { Collection } from "mongodb";
-import { Place } from "./types";
+import type { DbData } from "./types";
 
-// This will allow you to have one reference to each collection per app
-// TODO: Change the string literal union types later once we create actual db functions.
-// These are just examples, for now, to demonstrate how this should be used later.
-
-export async function getCollection(collection: "places" | "users"): Promise<Collection<Place>> {
-	let _col: Collection<Place> | undefined = undefined;
+export async function getCollection(
+	collection: "rating" | "user" | "place"
+): Promise<Collection<DbData>> {
+	const _col: Promise<Collection<DbData>> | undefined = undefined;
 
 	if (!_col) {
 		const db = await dbConnection();
-		_col = db.collection(collection);
+		return db.collection(collection);
 	}
 
 	return _col;
