39b30945e4eb954eaa175b9c4103e5483b9b68c2
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-10-22 23:00:23 -0400

    the api

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { getNearbyPlaces } from "./rest/getNearbyPlaces";
+import { getAllPlaceRatings, getRating, getRatingsFromUser } from "./rest/Ratings/getRatings";
 
 const app = express();
 const port = process.env.PORT || 3030;
@@ -9,6 +10,15 @@ const port = process.env.PORT || 3030;
 dotenv.config();
 
 app.get("/getNearbyPlaces", getNearbyPlaces);
+
+// get all ratings for certain place
+app.get("/getAllPlaceRatings/:id", getAllPlaceRatings);
+
+// get rating from database
+app.get("/getRating/:id", getRating);
+
+// get all the ratings that a user has given
+app.get("/getRatingsFromUser/:id", getRatingsFromUser);
 
 app.listen(port, () => {
 	console.log(`App listening at http://localhost:${port}`);
