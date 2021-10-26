7974ff27c03ee1fc77a066ee3650b920560502ef
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-10-26 17:41:52 -0400

    rating.ts now uses correct types

--- src/db/Rating/rating.ts
+++ src/db/Rating/rating.ts
@@ -1,14 +1,14 @@
 import { getCollection } from "../mongoCollections";
 import { InsertOneResult, ObjectId } from "mongodb";
-import type { DbData, Rating } from "../types";
+import type { Rating } from "../types";
 import type { Collection } from "mongodb";
 import type { Place as GooglePlaceID } from "@googlemaps/google-maps-services-js";
 import { updatePlace } from "../Place/place";
 
-const rating = getCollection("rating");
+const rating = getCollection<Rating>("rating");
 
-export async function addRating(ratingToAdd: Rating): Promise<DbData> {
-	const ratingCollection: Collection<DbData> = await rating;
+export async function addRating(ratingToAdd: Rating): Promise<Rating> {
+	const ratingCollection: Collection<Rating> = await rating;
 
 	const insertInfo: InsertOneResult<Rating> = await ratingCollection.insertOne(ratingToAdd);
 	if (insertInfo.acknowledged === false) throw "Error adding rating";
@@ -16,16 +16,15 @@ export async function addRating(ratingToAdd: Rating): Promise<DbData> {
 	const newID = insertInfo.insertedId;
 
 	// Every time an insertion is succesfful, we need to update the averages for the place in the Place collection
-	// this is type any for now because I can't figure out how to get it to work, sos
-	const insertedRating: any = await getRatingById(newID);
+	const insertedRating: Rating = await getRatingById(newID);
 
 	await updatePlace(insertedRating.placeID);
 
 	return insertedRating;
 }
 
-export async function getRatingById(id: ObjectId): Promise<DbData> {
-	const ratingCollection: Collection<DbData> = await rating;
+export async function getRatingById(id: ObjectId): Promise<Rating> {
+	const ratingCollection: Collection<Rating> = await rating;
 
 	const ratingReturned = await ratingCollection.findOne({ _id: id });
 	if (ratingReturned === null) throw "Sorry, no rating exists with that ID";
@@ -33,8 +32,8 @@ export async function getRatingById(id: ObjectId): Promise<DbData> {
 	return ratingReturned;
 }
 
-export async function getAllRatingsForPlace(id: GooglePlaceID["place_id"]): Promise<Array<DbData>> {
-	const ratingCollection: Collection<DbData> = await rating;
+export async function getAllRatingsForPlace(id: GooglePlaceID["place_id"]): Promise<Array<Rating>> {
+	const ratingCollection: Collection<Rating> = await rating;
 
 	const allRatings = await ratingCollection.find({ placeID: id }).toArray();
 	if (allRatings === null) throw "Sorry, no ratings exist for that place";
@@ -42,8 +41,8 @@ export async function getAllRatingsForPlace(id: GooglePlaceID["place_id"]): Prom
 	return allRatings;
 }
 
-export async function getAllRatingsFromUser(userId: ObjectId): Promise<Array<DbData>> {
-	const ratingCollection: Collection<DbData> = await rating;
+export async function getAllRatingsFromUser(userId: ObjectId): Promise<Array<Rating>> {
+	const ratingCollection: Collection<Rating> = await rating;
 
 	const allRatings = await ratingCollection.find({ userID: userId }).toArray();
 	if (allRatings === null) throw "Sorry, no ratings exist for that place";
