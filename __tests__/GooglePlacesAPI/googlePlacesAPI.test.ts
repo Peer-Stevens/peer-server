24365131d5ec5da2f3c220871434bd5862c138ca
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-11-12 18:14:26 -0500

    fixes to googlePlacesAPI.test.ts

--- __tests__/GooglePlacesAPI/googlePlacesAPI.test.ts
+++ __tests__/GooglePlacesAPI/googlePlacesAPI.test.ts
@@ -1,31 +1,22 @@
-import { Place } from "@googlemaps/google-maps-services-js";
-import axios from "axios";
+import request from "supertest";
+import { app, server } from "../../src/server";
+
+afterAll(() => {
+	server.close();
+});
 
 describe("Google Places API", () => {
 	it("/getNearbyPlaces returns nearby places", async () => {
-		let nearbyPlaces!: Place;
-		try {
-			const { data } = await axios.get<Place>(
-				"http://localhost:3030/getNearbyPlaces?latitude=40.743462&longitude=-74.029068"
-			);
-			nearbyPlaces = data;
-		} catch (e) {
-			console.log(e);
-		}
-
-		expect(nearbyPlaces).toBeDefined();
+		await request(app)
+			.get("/getNearbyPlaces?latitude=40.743462&longitude=-74.029068")
+			.expect("Content-Type", /json/)
+			.expect(200);
 	});
 	it("/getPlacePhoto returns an image", async () => {
-		let imageStatus!: number;
-		try {
-			const { status } = await axios.get<Place>(
-				"http://localhost:3030/getPlacePhoto/Aap_uEBlESKHx9XSFwCA1Mq9vXzyDYJt746bT6avBZUtHtquMh-U8exyK2bhs8gF7kXMcQIPS0B0c0HwCDhf6Nv22EgcyikJ_XARn6fGBprMHt3c6iJNTEQu0q6avzZuSobflgrWecAB2ayJD1JyMm75nGNqe3XPtayvYkJGxDn1mqvKon-k"
-			);
-			imageStatus = status;
-		} catch (e) {
-			console.log(e);
-		}
-		expect(imageStatus).toBeGreaterThanOrEqual(200);
-		expect(imageStatus).toBeLessThanOrEqual(299);
+		await request(app)
+			.get(
+				"/getPlacePhoto/Aap_uEBlESKHx9XSFwCA1Mq9vXzyDYJt746bT6avBZUtHtquMh-U8exyK2bhs8gF7kXMcQIPS0B0c0HwCDhf6Nv22EgcyikJ_XARn6fGBprMHt3c6iJNTEQu0q6avzZuSobflgrWecAB2ayJD1JyMm75nGNqe3XPtayvYkJGxDn1mqvKon-k"
+			)
+			.expect(200);
 	});
 });
