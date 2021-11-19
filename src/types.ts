ee496d722f156c0b7dd5ad9cabdf7b52135eaa50
David Carpenter <carpenterd777@gmail.com>
2021-11-19 15:16:43 -0500

    Add status code and constants

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,8 @@
 export class AuthenticationError extends Error {}
+
+// Constants
+export const UserCreatedJSON = { status: "User successfully created" };
+export const RatingCreatedJSON = { status: "Rating successfully created" };
+export const MalformedRequestErrorJSON = { error: "Malformed request" };
+export const UnauthorizedErrorJSON = { error: "Not authorized to make this request" };
+export const ServerErrorJSON = { error: "Something went wrong" };
