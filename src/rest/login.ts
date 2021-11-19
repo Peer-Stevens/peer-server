1ce214262c5c36f5dd748e4ecb9eec4e142684e4
David Carpenter <carpenterd777@gmail.com>
2021-11-19 17:28:13 -0500

    Add error check in login endpoint

--- src/rest/login.ts
+++ src/rest/login.ts
@@ -1,11 +1,18 @@
 import { editUserInDb, getUserByEmailAndHash } from "../db/User/user";
 import { Request, Response } from "express";
-import { AuthenticationError } from "../types";
+import { AuthenticationError, ServerErrorJSON } from "../types";
 import { createHash } from "crypto";
 import StatusCode from "./status";
 import { User } from "../db/types";
 import { ObjectId } from "bson";
 
+/**
+ * REST endpoint to login with an existing account.
+ * Assumes that the AUTH_SEED environment variable is set.
+ * @param req HTTP request with an email and hash in the body.
+ * @param res HTTP response to be sent.
+ * @returns
+ */
 export const login = async (
 	req: Request<unknown, unknown, { email: string; hash: string }>,
 	res: Response
@@ -27,6 +34,14 @@ export const login = async (
 	const token = createHash("sha256")
 		.update(`${new Date().toISOString()}${process.env.AUTH_SEED}`)
 		.digest("hex");
-	await editUserInDb(user._id as ObjectId, { token: token, dateTokenCreated: new Date() });
+
+	try {
+		await editUserInDb(user._id as ObjectId, { token: token, dateTokenCreated: new Date() });
+	} catch (e) {
+		// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+		console.error(`login: The following error was thrown with the request body: ${req.body}`);
+		console.error(e);
+		res.status(StatusCode.INTERNAL_SERVER_ERROR).json(ServerErrorJSON);
+	}
 	res.status(StatusCode.OK).send(token);
 };
