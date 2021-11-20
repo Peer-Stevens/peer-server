b1aa4703e86c682146a7b1c5bfcdef9f1db06676
David Carpenter <carpenterd777@gmail.com>
2021-11-19 19:49:41 -0500

    update json responses to be more informative

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
 export class AuthenticationError extends Error {}
 
 // Constants
-export const UserCreatedJSON = { status: "User successfully created" };
-export const RatingCreatedJSON = { status: "Rating successfully created" };
-export const MalformedRequestErrorJSON = { error: "Malformed request" };
-export const UnauthorizedErrorJSON = { error: "Not authorized to make this request" };
-export const ServerErrorJSON = { error: "Something went wrong" };
+export const UserCreatedJSON = { status: "User successfully created." };
+export const RatingCreatedJSON = { status: "Rating successfully created." };
+export const MalformedRequestErrorJSON = { error: "This request is missing parameters." };
+export const UnauthorizedErrorJSON = {
+	error: "You are not authorized to make this request. Please authenticate and try again.",
+};
+export const ServerErrorJSON = {
+	error: "Something went wrong on the server. Please try again later.",
+};
