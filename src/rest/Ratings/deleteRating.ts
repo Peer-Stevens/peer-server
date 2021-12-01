0f71d0c98f89588d9438ccb55c5b305885e7dc14
David Carpenter <carpenterd777@gmail.com>
2021-12-01 16:36:46 -0500

    delete rating rest endpoint requires authentication

--- src/rest/Ratings/deleteRating.ts
+++ src/rest/Ratings/deleteRating.ts
@@ -1,18 +1,41 @@
 import type { Request, Response } from "express";
-import { deleteRatingFromDb } from "../../db/Rating/rating";
 import { ObjectId } from "mongodb";
+import { deleteRatingFromDb } from "../../db/Rating/rating";
 import StatusCode from "../status";
+import {
+	handleError,
+	isAuthenticated,
+	RatingDeletedJSON,
+	RatingDoesNotExistErrorJSON,
+	ServerErrorJSON,
+	UnauthorizedErrorJSON,
+} from "../util";
 
-export const deleteRating = async (req: Request, res: Response): Promise<void> => {
+type DeleteRatingRequestBody = { userID: string; token: string; _id: string };
+
+export const deleteRating = async (
+	req: Request<unknown, unknown, DeleteRatingRequestBody>,
+	res: Response
+): Promise<void> => {
+	const { userID, _id, token } = req.body;
+
 	try {
-		const didDelete = await deleteRatingFromDb(new ObjectId(req.params.id));
+		if (!(await isAuthenticated(userID, token))) {
+			console.warn(
+				`deleteRating: logged-in user did not match provided token when trying to add rating`
+			);
+			res.status(StatusCode.UNAUTHORIZED).json(UnauthorizedErrorJSON);
+			return;
+		}
+		const didDelete = await deleteRatingFromDb(new ObjectId(_id));
 		if (didDelete === true) {
-			res.status(StatusCode.OK).json(didDelete);
+			res.status(StatusCode.OK).json(RatingDeletedJSON);
 			return;
 		} else {
-			res.status(StatusCode.BAD_REQUEST).json({ error: "Rating does not exist" });
+			res.status(StatusCode.BAD_REQUEST).json(RatingDoesNotExistErrorJSON);
 		}
 	} catch (e) {
-		res.status(StatusCode.INTERNAL_SERVER_ERROR).json(e);
+		handleError<DeleteRatingRequestBody>(e, "deleteRating", req, res);
+		res.status(StatusCode.INTERNAL_SERVER_ERROR).json(ServerErrorJSON);
 	}
 };
