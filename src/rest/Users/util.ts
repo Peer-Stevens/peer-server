a91a6293b41748453789d8cc519f0ea7b81bc3d5
David Carpenter <carpenterd777@gmail.com>
2021-12-01 20:09:57 -0500

    Add doc comments to  and

--- src/rest/Users/util.ts
+++ src/rest/Users/util.ts
@@ -4,6 +4,17 @@ import { DbOperationError } from "../../errorClasses";
 import { handleError } from "../util";
 import { getUserByID } from "../../db/User/user";
 
+/**
+ * Checks if the passed user exists. For use in a REST endpoint function;
+ * this function requires the request and response objects for the endpoint.
+ *
+ * Calls `getUserByID`. Calls `handleError` if something goes wrong.
+ * @param userID the user's bson ID as a string
+ * @param endPointName the name of the endpoint this is being called in
+ * @param req the endpoint's request object
+ * @param res the endpoint's response object
+ * @returns true if the user exists in the database
+ */
 export const userExists = async <T>(
 	userID: string,
 	endPointName: string,
