eebf63ab617429bbbed37bc302cac1d7259aa1d5
David Carpenter <carpenterd777@gmail.com>
2021-12-05 10:41:41 -0500

    add successful get all places test

--- __tests__/place.test.ts
+++ __tests__/place.test.ts
@@ -1,122 +1,112 @@
-import { Place } from "../src/db/types";
-import { dbConnection } from "../src/db/mongoConnection";
-import { getPlaceByID, addPlace } from "../src/db/Place/place";
-import { MongoServerError } from "mongodb";
+import { Collection, MongoClient } from "mongodb";
+import type { Place as GooglePlaceID } from "@googlemaps/google-maps-services-js";
+import { getCollection } from "../src/db/mongoCollections";
+import type { Place } from "../src/db/types";
+import { getAllPlaces } from "../src/db/Place/place";
 
-beforeAll(async () => {
-	const { _db, _connection } = await dbConnection();
-	await _db.dropDatabase();
-	await _connection.close();
+// getCollection mock
+jest.mock("../src/db/mongoCollections");
+type mockGetCollectionSignature = <T>(
+	collection: "user" | "rating" | "place"
+) => Promise<{ _col: Partial<Collection<T>>; _connection: Partial<MongoClient> }>;
+const mockGetCollection = getCollection as jest.MockedFunction<mockGetCollectionSignature>;
 
-	try {
-		await addPlace({
-			_id: "faketestid1",
-			avgBraille: null,
-			avgFontReadability: null,
-			avgGuideDogFriendly: null,
-			avgNavigability: null,
-			avgStaffHelpfulness: null,
-		});
-	} catch (e) {
-		if (e instanceof MongoServerError) {
-			console.log("MONGOSERVERERROR: Something went wrong while trying to connect to Mongo");
-		} else {
-			throw e;
+// mock adding to a collection
+let mockCollection: Place[];
+const mockInsertOne = jest.fn().mockImplementation((place: Place) => {
+	mockCollection.push(place);
+	return { acknowledged: true };
+});
+const mockFindOne = jest
+	.fn()
+	.mockImplementation(({ _id: id }: { _id: GooglePlaceID["place_id"] }) => {
+		return mockCollection.find(value => id === value._id);
+	});
+const mockFind = jest.fn();
+const mockUpdateOne = jest
+	.fn()
+	.mockImplementation(
+		(
+			{ _id: id }: { _id: GooglePlaceID["place_id"] },
+			{ $set: newPlace }: { $set: Partial<Place> }
+		) => {
+			const old = mockCollection.find(value => id === value._id);
+			mockCollection.pop();
+			mockCollection.push({
+				_id: id,
+				avgBraille:
+					newPlace.avgBraille !== undefined
+						? newPlace.avgBraille
+						: (old?.avgBraille as number),
+				avgFontReadability:
+					newPlace.avgFontReadability !== undefined
+						? newPlace.avgFontReadability
+						: (old?.avgFontReadability as number),
+				avgStaffHelpfulness:
+					newPlace.avgStaffHelpfulness !== undefined
+						? newPlace.avgStaffHelpfulness
+						: (old?.avgStaffHelpfulness as number),
+				avgNavigability:
+					newPlace.avgNavigability !== undefined
+						? newPlace.avgNavigability
+						: (old?.avgNavigability as number),
+				avgGuideDogFriendly:
+					newPlace.avgGuideDogFriendly !== undefined
+						? newPlace.avgGuideDogFriendly
+						: (old?.avgGuideDogFriendly as number),
+			});
+			return { acknowledged: true };
 		}
-	}
-
-	try {
-		await addPlace({
-			_id: "faketestid4",
-			avgBraille: null,
-			avgFontReadability: null,
-			avgGuideDogFriendly: null,
-			avgNavigability: null,
-			avgStaffHelpfulness: null,
-		});
-	} catch (e) {
-		if (e instanceof MongoServerError) {
-			console.log("MONGOSERVERERROR: Something went wrong while trying to connect to Mongo");
-		} else {
-			throw e;
-		}
-	}
+	);
+const mockClose = jest.fn();
+mockGetCollection.mockResolvedValue({
+	_col: {
+		insertOne: mockInsertOne,
+		findOne: mockFindOne,
+		updateOne: mockUpdateOne,
+		find: mockFind,
+	},
+	_connection: { close: mockClose },
 });
 
-describe("Place REST endpoints", () => {
-	it("gets place", async () => {
-		let place!: Place;
-		try {
-			place = await getPlaceByID("faketestid1");
-		} catch (e) {
-			if (e instanceof MongoServerError) {
-				console.log(
-					"MONGOSERVERERROR: Something went wrong while trying to connect to Mongo"
-				);
-			} else {
-				throw e;
-			}
-		}
+// mock data
 
-		expect(place).toMatchObject<Place>({
-			_id: "faketestid1",
-			avgBraille: null,
-			avgFontReadability: null,
-			avgGuideDogFriendly: null,
-			avgNavigability: null,
-			avgStaffHelpfulness: null,
-		});
-	});
-	it("adds place to database", async () => {
-		let place!: Place;
+const mockPlace1: Place = {
+	_id: "wumpus",
+	avgBraille: null,
+	avgFontReadability: null,
+	avgGuideDogFriendly: null,
+	avgNavigability: null,
+	avgStaffHelpfulness: null,
+};
 
-		try {
-			place = await addPlace({
-				_id: "faketestid3",
-				avgBraille: null,
-				avgFontReadability: null,
-				avgGuideDogFriendly: null,
-				avgNavigability: null,
-				avgStaffHelpfulness: null,
-			});
-		} catch (e) {
-			if (e instanceof MongoServerError) {
-				console.log(
-					"MONGOSERVERERROR: Something went wrong while trying to connect to Mongo"
-				);
-			} else {
-				throw e;
-			}
-		}
+const mockPlace2: Place = {
+	_id: "andrewshouse",
+	avgBraille: 5,
+	avgFontReadability: 5,
+	avgGuideDogFriendly: 5,
+	avgNavigability: 5,
+	avgStaffHelpfulness: 5,
+};
 
-		expect(place).toMatchObject<Place>({
-			_id: "faketestid3",
-			avgBraille: null,
-			avgFontReadability: null,
-			avgGuideDogFriendly: null,
-			avgNavigability: null,
-			avgStaffHelpfulness: null,
-		});
-	});
-	it("throws error when duplicate place is added", async () => {
-		expect.assertions(1);
-		return await addPlace({
-			_id: "faketestid4",
-			avgBraille: null,
-			avgFontReadability: null,
-			avgGuideDogFriendly: null,
-			avgNavigability: null,
-			avgStaffHelpfulness: null,
-		}).catch(e => {
-			if (e instanceof MongoServerError) {
-				console.log(
-					"MONGOSERVERERROR: Something went wrong while trying to connect to Mongo"
-				);
-			} else {
-				expect(e).toEqual(
-					"That place already exists in the database and cannot be added again."
-				);
-			}
-		});
+beforeEach(() => {
+	mockCollection = [];
+	mockInsertOne.mockClear();
+	mockUpdateOne.mockClear();
+	mockFindOne.mockClear();
+	mockClose.mockClear();
+	mockFind.mockClear();
+});
+
+describe("Place-related database function tests", () => {
+	it("successfully gets all the places", async () => {
+		mockFind.mockReturnValue({ toArray: () => mockCollection });
+		mockCollection.push(mockPlace1);
+		mockCollection.push(mockPlace2);
+
+		const foundPlaces = await getAllPlaces();
+
+		expect(mockClose).toHaveBeenCalled();
+		expect(foundPlaces).toEqual(mockCollection);
 	});
 });
