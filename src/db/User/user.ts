133f471eb8d245a2cfd309f3aaa2940df2cefd87
David Carpenter <carpenterd777@gmail.com>
2021-12-05 10:24:09 -0500

    add throws clauses to each doc comment

--- src/db/User/user.ts
+++ src/db/User/user.ts
@@ -7,6 +7,7 @@ import { AuthenticationError, DbOperationError } from "../../errorClasses";
  * Adds a user to the remote collection. Intended to be called when
  * a user creates an account.
  * @param userToAdd the new user to add
+ * @throws if there is a problem adding the user to remote collection
  * @returns the user that has just been added
  */
 export async function addUserToDb(userToAdd: User): Promise<User> {
@@ -22,6 +23,7 @@ export async function addUserToDb(userToAdd: User): Promise<User> {
 /**
  * Finds a user from the remote collection.
  * @param id the id of the user to find
+ * @throws if there is no user with the passsed id
  * @returns the user
  */
 export async function getUserByID(id: ObjectId): Promise<User> {
@@ -37,6 +39,7 @@ export async function getUserByID(id: ObjectId): Promise<User> {
 /**
  * Finds a user from the remote collection by their email.
  * @param email the email of the user to find
+ * @throws if there no user with the passed email address
  * @returns the user
  */
 export const getUserByEmailOnly = async (email: string): Promise<User> => {
@@ -44,7 +47,7 @@ export const getUserByEmailOnly = async (email: string): Promise<User> => {
 	const userReturned = await _col.findOne({ email: email });
 	await _connection.close();
 	if (userReturned === null)
-		throw new AuthenticationError(`No user exists with the username ${email}`);
+		throw new AuthenticationError(`No user exists with the email address ${email}`);
 	return userReturned;
 };
 
@@ -54,6 +57,7 @@ export const getUserByEmailOnly = async (email: string): Promise<User> => {
  * for this to return.
  * @param email the email of the user to find
  * @param hash the hash of the user to find
+ * @throws if there is no user with the passed combination of email and hash
  * @returns the user
  */
 export const getUserByEmailAndHash = async (email: string, hash: string): Promise<User> => {
@@ -61,7 +65,9 @@ export const getUserByEmailAndHash = async (email: string, hash: string): Promis
 	const userReturned = await _col.findOne({ email: email, hash: hash });
 	await _connection.close();
 	if (userReturned === null)
-		throw new AuthenticationError(`No user exists with the username ${email} and hash ${hash}`);
+		throw new AuthenticationError(
+			`No user exists with the email address ${email} and hash ${hash}`
+		);
 	return userReturned;
 };
 
@@ -69,6 +75,7 @@ export const getUserByEmailAndHash = async (email: string, hash: string): Promis
  * Updates the fields of a user in the remote collection.
  * @param userId the id of ther user to edit
  * @param newUserFields the new fields for the user
+ * @throws if there was a problem editing the user in the remote collection
  * @returns the user as it now appears in the collection
  */
 export async function editUserInDb(userId: ObjectId, newUserFields: Partial<User>): Promise<User> {
