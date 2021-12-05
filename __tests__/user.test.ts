fb000181c1363c688837b4766c3223c7dc4591de
David Carpenter <carpenterd777@gmail.com>
2021-12-04 22:42:40 -0500

    add tests for gettting email and hash

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -1,5 +1,10 @@
 import { Collection, MongoClient, ObjectId } from "mongodb";
-import { addUserToDb, getUserByEmailOnly, getUserByID } from "../src/db/User/user";
+import {
+	addUserToDb,
+	getUserByEmailAndHash,
+	getUserByEmailOnly,
+	getUserByID,
+} from "../src/db/User/user";
 import { getCollection } from "../src/db/mongoCollections";
 import { User } from "../src/db/types";
 import { AuthenticationError, DbOperationError } from "../src/errorClasses";
@@ -113,6 +118,34 @@ describe("User-related database function tests", () => {
 		// user has not been added!
 
 		getUserByEmailOnly(mockUser.email).catch(e => {
+			expect(mockClose).toHaveBeenCalled();
+			expect(e).toBeInstanceOf(AuthenticationError);
+		});
+	});
+
+	it("successfully gets a user by their email and hash", async () => {
+		mockCollection.push(mockUser);
+		mockFindOne.mockImplementationOnce(
+			({ email: email, hash: hash }: { email: string; hash: string }) => {
+				return mockCollection.find(value => email === value.email && hash === value.hash);
+			}
+		);
+
+		const foundUser = await getUserByEmailAndHash(mockUser.email, mockUser.hash);
+
+		expect(mockClose).toHaveBeenCalled();
+		expect(foundUser).toEqual(mockUser);
+	});
+
+	it("throws an error when getting a user by email that has not been added", () => {
+		mockFindOne.mockImplementationOnce(
+			({ email: email, hash: hash }: { email: string; hash: string }) => {
+				return mockCollection.find(value => email === value.email && hash === value.hash);
+			}
+		);
+		// user has not been added!
+
+		getUserByEmailAndHash(mockUser.email, mockUser.hash).catch(e => {
 			expect(mockClose).toHaveBeenCalled();
 			expect(e).toBeInstanceOf(AuthenticationError);
 		});
